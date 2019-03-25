cabd7fe1f8a54f01373fed831f66a7925dbebc53
vipinAug17 <vipinaug17@gmail.com>
2019-03-25 18:53:19 +0530

    Updates

---
+++ precache-manifest.fc8b8cfa32f8b24030914209182809a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2f23047b078009b94f5f",
+    "url": "/demo/static/js/runtime~main.96f7b30f.js"
+  },
+  {
+    "revision": "349b68aa4f320ec29b7f",
+    "url": "/demo/static/js/main.c6dcc473.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/demo/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "349b68aa4f320ec29b7f",
+    "url": "/demo/static/css/main.64530a8f.chunk.css"
+  },
+  {
+    "revision": "4919a3618c509f45c0d674b373cf70b6",
+    "url": "/demo/index.html"
+  }
+];
